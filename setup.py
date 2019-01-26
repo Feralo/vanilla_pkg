d5136bad3160963afd11607b033c0c0f4442a0bd
Noah Spahn <noah-de@users.noreply.github.com>
2019-01-26 11:29:44 -0800

    vanilla-pkg as the name (and URL)

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example_pkg",
+    name="vanilla-pkg",
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
     description="A small example package",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/Feralo/vanilla-pkg",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
