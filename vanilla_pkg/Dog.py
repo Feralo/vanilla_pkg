3e8851b270cd1adabaa1bfe9d6f1bfd006cfdd4e
noah-de <noah.de@gmail.com>
2019-03-16 11:15:57 -0700

    Dog class to provide the class that is tested

---
+++ vanilla_pkg/Dog.py
@@ -0,0 +1,10 @@
+class Dog:
+    
+    kind = 'canine'
+    tricks = []
+    
+    def __init__(self, name):
+        self.name = name
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
