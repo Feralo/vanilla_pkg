c8a05ac79f8cf1b3bdb3ff1a82f42d90aa3bfb02
noah-de <noah.de@gmail.com>
2019-03-16 11:27:28 -0700

    Changing the scope of tricks (instance variable instead of class variable)

--- vanilla_pkg/Dog.py
+++ vanilla_pkg/Dog.py
@@ -1,10 +1,10 @@
 class Dog:
     
     kind = 'canine'
-    tricks = []
     
     def __init__(self, name):
         self.name = name
+        self.tricks = []
 
     def add_trick(self, trick):
         self.tricks.append(trick)
