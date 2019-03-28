9ba762d761137ae69350071d9b0da913f147f83d
noah-de <noah.de@gmail.com>
2019-03-28 16:27:26 -0700

    Test all Mock conditions

--- vanilla_pkg/tests/mock_test.py
+++ vanilla_pkg/tests/mock_test.py
@@ -1,5 +1,5 @@
 from unittest.mock import Mock, patch
-from nose.tools import assert_true, assert_is_not_none
+from nose.tools import assert_true, assert_is_none, assert_is_not_none
 
 from vanilla_pkg import get_todos
 
@@ -7,9 +7,11 @@ from vanilla_pkg import get_todos
 @patch("vanilla_pkg.Mocks.requests.get")
 def test_request_response(mock_get):
     mock_get.return_value.ok = True
-
-    # Call the service, which will send a request to the server.
     response = get_todos()
-
-    # If the request is sent successfully, then I expect a response to be returned.
     assert_is_not_none(response)
+
+@patch("vanilla_pkg.Mocks.requests.get")
+def test_request_response_not_ok(mock_get):
+    mock_get.return_value.ok = False
+    response = get_todos()
+    assert_is_none(response)
